3d9f7c1ef70a5f80d65c7729089ad1c938924a7b
Steven Klein <steven.p.klein@gmail.com>
2017-01-28 12:46:06 -0600

    assignment 1

---
+++ httpdocs/1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Assignment 1</title>
+        <style>
+            .php {
+                font-size: 2em;
+                font-style: italic;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <h1>PHP And HTML</h1>
+            <p>
+                Learning HTML is fun.<br>
+                Learning HTML is very easy.
+            </p>
+            <p class="php">
+                <?php echo "Learning PHP is fun.";?><br>
+                <?php echo "Learning PHP is very easy.";?>
+            </p>
+        </main>
+    </body>
+</html>
